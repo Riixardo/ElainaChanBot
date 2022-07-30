e1b4f3f84aa89173c99ae241ca485bf3b6d16bcc
Richard Chen <RicardoMonriguez2@gmail.com>
2022-07-30 06:07:44 +0000

    Tried adding music from tutorials, but failed due to "opus not loaded"

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 from replit import db
 from keep_alive import keep_alive
 from discord.ext import commands
+import youtube_dl
 
 client = commands.Bot(command_prefix='?')
 
@@ -77,8 +78,75 @@ def return_list(message_type):
 
 
 @client.command()
-async def foo(ctx, arg):
-    await ctx.send(arg)
+async def roll(ctx):
+    await ctx.send("I will do so at once Master")
+
+
+# @client.command()
+# async def play(ctx, url: str):
+#     song_there = os.path.isfile("song.mp3")
+#     try:
+#         if song_there:
+#             os.remove("song.mp3")
+#     except PermissionError:
+#         await ctx.send(
+#             "Wait for the current playing music to end or use the 'stop' command"
+#         )
+#         return
+
+#     voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+#     await voiceChannel.connect()
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+#     ydl_opts = {
+#         'format':
+#         'bestaudio/best',
+#         'postprocessors': [{
+#             'key': 'FFmpegExtractAudio',
+#             'preferredcodec': 'mp3',
+#             'preferredquality': '192',
+#         }],
+#     }
+#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#         ydl.download([url])
+#     for file in os.listdir("./"):
+#         if file.endswith(".mp3"):
+#             os.rename(file, "song.mp3")
+#     voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+# @client.command()
+# async def leave(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     if voice.is_connected():
+#         await voice.disconnect()
+#     else:
+#         await ctx.send("I am not in a VC")
+
+# @client.command()
+# async def pause(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     if voice.is_playing():
+#         voice.pause()
+#     else:
+#         await ctx.send("No audio is playing")
+
+# @client.command()
+# async def resume(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     if voice.is_paused():
+#         voice.resume()
+#     else:
+#         await ctx.send("Audio is not paused")
+
+# async def stop(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     voice.stop()
+
+
+@client.command()
+async def quote(ctx):
+    quote = get_quote()
+    await ctx.send(quote)
 
 
 @client.command()
@@ -115,10 +183,6 @@ async def on_message(message):
     msg = message.content
     if message.author == client.user:
         return
-    if msg.startswith('?quote'):
-        quote = get_quote()
-        await message.channel.send(quote)
-        return
     if (check_message_add("encouragements", msg)):
         await message.channel.send("New message added.")
         return
@@ -139,10 +203,10 @@ async def on_message(message):
         ))
         return
     if msg.startswith("?customlist encouragements"):
-        await message.channel.send(return_list("encouragements"))
+        await message.channel.send(list(return_list("encouragements")))
         return
     if msg.startswith("?customlist greetings"):
-        await message.channel.send(return_list("greetings"))
+        await message.channel.send(list(return_list("greetings")))
         return
     if msg.startswith("?responding "):
         value = msg.split("?responding ", 1)[1]
